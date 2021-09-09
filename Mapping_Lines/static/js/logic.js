f7b0ebbb69b303f2a320215ebe508ee0a29b4b2d
ethiry99 <eric.thiry99@gmail.com>
2021-09-09 07:54:17 -0500

    added extra city to airline path

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,26 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [37.6213, -122.3790],
+    [30.18999924, -97.668663992],
+    [43.678524, -79.6291291306445],
+    [46.107332904, -64.673830638],
+    [40.641766, -73.780968]
+  ];
+  
+  // Create a polyline using the line coordinates and make the line red.
+  // Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+    color: "blue",
+    weight:4,
+    opacity:0.5,
+    dashArray:'5,8'
+ }).addTo(map);
 
 // Create the map object with a center and zoom level.
 //This method is useful when we need to add multiple tile layers, 
@@ -19,7 +38,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
